70e0778a8aea1a1ba55689dbc984c80acb9c610a
Justin Barry <justin.barry.e@gmail.com>
2017-11-06 14:44:02 -0500

    Adding test module

---
+++ maintest.py
@@ -0,0 +1,16 @@
+import unittest, random
+
+
+class TestProblem2(unittest.TestCase):
+    def setUp(self):
+        pass
+
+    def test_heap_size_one(self):
+        pass
+
+    def test_insert(self):
+        pass
+
+
+if __name__ == '__main__':
+    unittest.main()
